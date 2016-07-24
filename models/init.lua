ecbb7f0ffa8e669a100f61541eae83feec85b1cd
Remi Cadene <remi.cadene@icloud.com>
2016-07-24 14:22:01 +0200

    Add vgg16

--- models/init.lua
+++ models/init.lua
@@ -3,6 +3,7 @@ local vision = require 'torchnet-vision.env'
 local models = {}
 vision.models = models
 
+models.vgg16       = require 'torchnet-vision.models.vgg16'
 models.inceptionv3 = require 'torchnet-vision.models.inceptionv3'
 
 return models
--- models/vgg16.lua
+++ models/vgg16.lua
@@ -1,49 +1,55 @@
 local argcheck = require 'argcheck'
 
-local inceptionv3 = {}
+local vgg16 = {}
 
-inceptionv3.__download = argcheck{
+vgg16.__download = argcheck{
    {name='filename', type='string'},
    call =
       function(filename)
          os.execute('mkdir -p '..paths.dirname(filename)..';'
-                  ..'wget http://webia.lip6.fr/~cadene/Downloads/InceptionV3.t7'
+                  ..'wget http://webia.lip6.fr/~cadene/Downloads/vgg16.t7'
                       ..' -O '..filename)
       end
 }
 
-inceptionv3.load = argcheck{
+vgg16.load = argcheck{
    {name='filename', type='string'},
    call =
       function(filename)
          if not path.exists(filename) then
-            inceptionv3.__download(filename)
+            vgg16.__download(filename)
          end
          return torch.load(filename)
       end
 }
 
-inceptionv3.loadFinetuning = argcheck{
+vgg16.loadFinetuning = argcheck{
    {name='filename', type='string'},
    {name='nclasses', type='number'},
    {name='ftfactor', type='number', default=10},
+   {name='layerid',  type='number', default=38},
    call =
-      function(filename, nclasses, ftfactor)
-         local net = inceptionv3.load(filename)
+      function(filename, nclasses, ftfactor, layerid)
+         local net = vgg16.load(filename)
          net:remove() -- nn.SoftMax
          net:remove() -- nn.Linear
-         net:add(nn.GradientReversal(-1*ftfactor))
          net:add(nn.Linear(2048, nclasses))
+         for i=net:size(), layerid+1, -1 do
+            net:get(i):reset()
+         end
+         net:insert(nn.GradientReversal(-1*ftfactor), layerid)
+         end
          return net
       end
 }
 
-inceptionv3.loadExtracting = argcheck{
+vgg16.loadExtracting = argcheck{
    {name='filename', type='string'},
-   {name='layerid',  type='string'},
+   {name='layerid',  type='number'},
    call =
       function(filename, layerid)
-         local net = inceptionv3.load(filename)
+         local net = vgg16.load(filename)
+         end
          for i=net:size(), layerid+1, -1 do
             net:remove()
          end
@@ -52,9 +58,9 @@ inceptionv3.loadExtracting = argcheck{
       end
 }
 
-inceptionv3.colorMode = 'RGB'
-inceptionv3.inputSize = {3, 299, 299}
-inceptionv3.mean = torch.Tensor{128, 128, 128}
-inceptionv3.std  = torch.Tensor{128, 128, 128}
+vgg16.colorMode = 'BGR'
+vgg16.inputSize = {3, 221, 221}
+vgg16.mean = torch.Tensor{123.68, 116.779, 103.939}
+-- no std
 
-return inceptionv3
+return vgg16
