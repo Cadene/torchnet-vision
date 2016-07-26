ed856894a59fb64493c053ced811e14fc08f74ad
Remi Cadene <remi.cadene@icloud.com>
2016-07-26 18:10:14 +0200

    Fix require mit67

--- datasets/init.lua
+++ datasets/init.lua
@@ -5,5 +5,6 @@ vision.datasets = datasets
 
 datasets.utils       = require 'torchnet-vision.datasets.utils'
 datasets.upmcfood101 = require 'torchnet-vision.datasets.upmcfood101'
+datasets.mit67       = require 'torchnet-vision.datasets.mit67'
 
 return datasets
